60e4cfc2c438ee096f9421f62a369b6321427b7e
CodeWithSomesh <codewithsomesh@gmail.com>
2023-08-20 21:06:39 +0800

    95% done with this project

--- script.js
+++ script.js
@@ -2,15 +2,6 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const hamburgerEdge = document.querySelector(".hamburger-menu-edge")
 const navMenu = document.querySelector(".nav-menu");
-const heroImg = document.getElementById("hero-img")
-const eggImg = document.getElementById("egg-img")
-const cupImg = document.getElementById("cup-img")
-const graphicDesignImg = document.getElementById("graphic-design-img")
-const photographyImg = document.getElementById("photography-img")
-const mlkBottlesImg = document.getElementById("milk-bottles-img")
-const orangeImg = document.getElementById("orange-img")
-const coneImg = document.getElementById("cone-img")
-const sugarCubesImg = document.getElementById("sugar-cubes-img")
 
 
 // When the hamburger menu is clicked on, the hamburger menu animation will occur and nav-menu will appear
